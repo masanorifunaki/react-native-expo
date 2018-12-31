c039b44318df6f54ee1c7a5eb2209bec74f2bedf
舟木正憲 <funakimfunaki@gmail.com>
2018-12-31 15:47:39 +0900

    Buttonを表示する実装

--- App.js
+++ App.js
@@ -1,12 +1,15 @@
-import React                             from 'react';
-import { Image, StyleSheet, Text, View } from 'react-native';
+import React                                                       from 'react';
+import { Button, Image, StyleSheet, Text, TouchableOpacity, View } from 'react-native';
 
 export default class App extends React.Component {
   render() {
     return (
       <View style={styles.container}>
-        <Image style={ styles.image } source={ require('./assets/sample.png') }/>
+        <TouchableOpacity onPress={ () => console.warn('pressed!') }>
+          <Image style={ styles.image } source={ require('./assets/sample.png') }/>
+        </TouchableOpacity>
         <Text>Open up App.js to start working on your app!</Text>
+        <Button onPress={ () => console.warn('pressed!') } title={ 'Press' }/>
       </View>
     );
   }
